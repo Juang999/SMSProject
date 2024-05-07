4e4af853df6863d053da12e414e685569036c0c8
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-05-08 06:30:58 +0700

    update config and .env file

--- config/connection.js
+++ config/connection.js
@@ -2,24 +2,24 @@ require('dotenv').config()
 
 module.exports = {
     development: {
-        username: process.env.DEV_DB_USERNAME,
-        password: process.env.DEV_DB_PASSWORD,
-        database: process.env.DEV_DB_DATABASE,
-        host: process.env.DEV_DB_HOST,
-        dialect: process.env.DEV_DB_DIALECT
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
     },
     test: {
-        username: process.env.TEST_DB_USERNAME,
-        password: process.env.TEST_DB_PASSWORD,
-        database: process.env.TEST_DB_DATABASE,
-        host: process.env.TEST_DB_HOST,
-        dialect: process.env.TEST_DB_DIALECT
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
     },
     production: {
-        username: process.env.PRODUCTION_DB_USERNAME,
-        password: process.env.PRODUCTION_DB_PASSWORD,
-        database: process.env.PRODUCTION_DB_DATABASE,
-        host: process.env.PRODUCTION_DB_HOST,
-        dialect: process.env.PRODUCTION_DB_DIALECT
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
     }
 }
