bc04bf66a4549642ca00b3761a1f7f84e30e88bf
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-05-13 06:14:55 +0700

    create seeders for dummy

---
+++ seeders/20240509123241-UserHasRoleSeeder.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const moment = require('moment')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    return queryInterface.bulkInsert('UserHasRoles', [
+      {
+        user_id: 2,
+        role_id: 1,
+        createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+        updatedAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+      }, {
+        user_id: 3,
+        role_id: 2,
+        createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+        updatedAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+      }, {
+        user_id: 4,
+        role_id: 3,
+        createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+        updatedAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+      }, {
+        user_id: 5,
+        role_id: 4,
+        createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+        updatedAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+      }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    return queryInterface.bulkDelete('UserHasRoles', null, {})
+  }
+};
