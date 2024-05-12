5137a2852dabe829ac45032af06e35c288bc8f54
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-05-13 06:17:36 +0700

    update helper module

--- helper/Auth.js
+++ helper/Auth.js
@@ -1,32 +1,55 @@
-const {User} = require('../models')
+const {User, UserHasRole, Role, Sequelize} = require('../models')
 const {compare, compareSync} = require('bcrypt')
 
 class Auth {
-    static attempt = async (request) => {
+    attempt = async (request) => {
+        let dataUser = await this.findUser(request)
+
+        if (dataUser == null) return {status: false, data: null}
+
+        let RESULT_CHECK_PASSWORD = this.checkPassword(request.password, dataUser.dataValues.password)
+
+        return {
+            status: (RESULT_CHECK_PASSWORD == true) ? true : false,
+            data: (RESULT_CHECK_PASSWORD) ? dataUser.dataValues : []
+        }
+    }
+
+    checkPassword = (realPassword, hashedPassword) => {
+        return compareSync(realPassword, hashedPassword)
+    }
+
+    findUser = async (request) => {
         let dataUser = await User.findOne({
             attributes: [
                 'id',
                 'username',
                 'password',
                 'student_id',
-                'teacher_id'
+                'teacher_id',
+                [Sequelize.literal('"user_has_role"."role_id"'), 'role_id'],
+                [Sequelize.literal('"user_has_role->detail_role"."role_name"'), 'role_name']
+            ],
+            include: [
+                {
+                    model: UserHasRole,
+                    as: 'user_has_role',
+                    atttributes: [],
+                    include: [
+                        {
+                            model: Role,
+                            as: 'detail_role',
+                            attributes: []
+                        }
+                    ]
+                }
             ],
             where: {
                 username: request.username
             }
         })
 
-        return this.checkPassword(request.password, dataUser)
-    }
-
-    checkPassword = (realPassword, hashedPassword) => {
-        let RESULT_STATUS_PASSWORD = compareSync(realPassword, hashedPassword)
-
-        if (RESULT_STATUS_PASSWORD == true) {
-            throw new Error('ups anda menggunaakan fitur ini!')
-        } else {
-            return true
-        }
+        return dataUser
     }
 }
 
