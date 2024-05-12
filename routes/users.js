f2f3645fecba24c4274331fecb79a4cebc3b7722
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-05-13 06:20:52 +0700

    add new API for login

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const {AuthController} = require('../app/Controller/Controller.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* POST login user. */
+router.post('/login', AuthController.login);
 
 module.exports = router;
