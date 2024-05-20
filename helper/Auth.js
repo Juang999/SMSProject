05eb0cb8c663884c504ae217b111f0b0cc5577a1
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-05-20 07:15:24 +0700

    modify file Auth.js in helper folder

--- helper/Auth.js
+++ helper/Auth.js
@@ -1,25 +1,26 @@
 const {User, UserHasRole, Role, Sequelize} = require('../models')
 const {compare, compareSync} = require('bcrypt')
+const Bcrypt = require('./Bcrypt');
 
 class Auth {
     attempt = async (request) => {
-        let dataUser = await this.findUser(request)
+        let dataUser = await this.findUser(request.username)
 
         if (dataUser == null) return {status: false, data: null}
 
-        let RESULT_CHECK_PASSWORD = this.checkPassword(request.password, dataUser.dataValues.password)
+        let RESULT_CHECK_PASSWORD = this.checkPassword(request.password, dataUser.password)
 
         return {
-            status: (RESULT_CHECK_PASSWORD == true) ? true : false,
-            data: (RESULT_CHECK_PASSWORD) ? dataUser.dataValues : []
+            status: (RESULT_CHECK_PASSWORD) ? true : false,
+            data: (RESULT_CHECK_PASSWORD) ? dataUser : []
         }
     }
 
     checkPassword = (realPassword, hashedPassword) => {
-        return compareSync(realPassword, hashedPassword)
+        return compareSync(realPassword, hashedPassword);
     }
 
-    findUser = async (request) => {
+    findUser = async (username) => {
         let dataUser = await User.findOne({
             attributes: [
                 'id',
@@ -45,11 +46,19 @@ class Auth {
                 }
             ],
             where: {
-                username: request.username
+                username: username
             }
         })
 
-        return dataUser
+        return (dataUser) ? {
+            id: Bcrypt.AESEncrypt(dataUser.dataValues.id),
+            username: dataUser.dataValues.username,
+            password: dataUser.dataValues.password,
+            student_id: dataUser.dataValues.student_id,
+            teacher_id: dataUser.dataValues.teacher_id,
+            role_id: Bcrypt.AESEncrypt(dataUser.dataValues.role_id),
+            role_name: dataUser.dataValues.role_name,
+        } : null;
     }
 }
 
