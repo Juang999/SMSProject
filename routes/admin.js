8e9349f0fc3a2654970b362064626e42648290e0
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-05-22 08:34:50 +0700

    add new route to delete data teacher via admin

--- routes/admin.js
+++ routes/admin.js
@@ -15,5 +15,6 @@ router.get('/teacher', [Authorization], TeacherController.index)
 router.get('/teacher/:id/detail', [Authorization], TeacherController.show)
 router.put('/teacher/:id/update', [Authorization], TeacherController.update)
 router.post('/teacher/create', [Authorization], TeacherController.createDataTeacher)
+router.delete('/teacher/:id/delete', [Authorization], TeacherController.delete)
 
 module.exports = router
