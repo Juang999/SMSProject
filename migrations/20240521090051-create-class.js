628c3b2c9810db83e16de39692fe312853e1326c
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-05-22 08:33:40 +0700

    new model for class subfeature

---
+++ migrations/20240521090051-create-class.js
@@ -0,0 +1,50 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Classes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      class: {
+        type: Sequelize.STRING
+      },
+      class_code: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'CodeMasters',
+            schema: 'public'
+          },
+          key: 'id'
+        }
+      },
+      grade: {
+        type: Sequelize.STRING
+      },
+      is_active: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Classes');
+  }
+};
