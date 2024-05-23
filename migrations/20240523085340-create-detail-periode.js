76f802957cdcbd2e0b63d0fb4e4327c506b5f4de
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-05-23 20:03:43 +0700

    create new table for detail periode

---
+++ migrations/20240523085340-create-detail-periode.js
@@ -0,0 +1,54 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('DetailPeriodes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      semester_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'CodeMasters',
+            schema: 'public'
+          },
+          key: 'id'
+        }
+      },
+      periode_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'Periodes',
+            schema: 'public'
+          },
+          key: 'id'
+        }
+      },
+      start_date: {
+        type: Sequelize.DATEONLY
+      },
+      end_date: {
+        type: Sequelize.DATEONLY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('DetailPeriodes');
+  }
+};
