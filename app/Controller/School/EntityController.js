8dcd9fe40e4d8c796b014639a07e42d1ba58e65f
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-05-31 14:54:06 +0700

    add new feature for entity

--- app/Controller/School/EntityController.js
+++ app/Controller/School/EntityController.js
@@ -1,65 +1,307 @@
 const {
     Entity,
-    DetailEntity
+    CodeMaster,
+    DetailEntity,
+    Sequelize,sequelize,
 } = require('../../../models')
 
 class EntityController {
-    index = async () => {
-        let dataEntity = await Entity.findAll()
-
-        return dataEntity;
+    index = (req, res) => {
+        Entity.findAll()
+            .then(result => {
+                res.status(200)
+                    .json({
+                        status: 'success',
+                        data: result,
+                        error: null
+                    })
+            })
+            .catch(err => {
+                res.status(400)
+                    .json({
+                        status: 'failed',
+                        data: null,
+                        error: err.message
+                    })
+            })
     }
 
-    show = async (id) => {
-        let {data, status} = await this.checkExistanceData(id)
+    show = async (req, res) => {
+        try {
+            let {status} = await this.checkExistanceData(req.params.id)
 
-        if (status == false) {
-            return false;
-        }
+            if (status == false) {
+                res.status(404)
+                    .json({
+                        status: 'not found!',
+                        data: null,
+                        error: 'not found!'
+                    })
 
-        let result = await Entity.findOne({
-            attributes: [
-                'id',
-                'entity_name',
-                'is_active'
-            ],
-            include: [
-                {
-                    model: DetailEntity,
-                    as: 'detail_entity',
-                    attributes: ['id', 'entity_id', 'location_id', 'is_active']
-                }
-            ],
-            where: {
-                id: id
+                return;
             }
-        })
 
-        return result;
+            let result = await Entity.findOne({
+                attributes: [
+                    'id',
+                    'entity_name',
+                    'is_active'
+                ],
+                include: [
+                    {
+                        model: DetailEntity,
+                        as: 'detail_entity',
+                        attributes: [
+                            'id', 
+                            'entity_id', 
+                            'location_id', 
+                            [Sequelize.literal(`"detail_entity->location"."code_name"`), 'location_name'],
+                            'is_active',
+                        ],
+                        include: [
+                            {
+                                model: CodeMaster,
+                                as: 'location',
+                                attributes: []
+                            }
+                        ]
+                    }
+                ],
+                where: {
+                    id: req.params.id
+                }
+            })
+
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: result,
+                    error: null
+                })
+        } catch (error) {
+            res.status(400)
+                .json({
+                    status: 'failed',
+                    data: null,
+                    error: error.message
+                })
+        }
     }
 
-    store = async (req) => {
-        let result = await Entity.create({
+    store = (req, res) => {
+        Entity.create({
             entity_name: req.body.entity_name,
-            is_active: true
+            is_active: true,
+            province: req.body.province,
+            city_regency: req.body.city_regency,
+            sub_regency: req.body.sub_regency,
+            address: req.body.address,
         })
-
-        return result
+        .then(result => {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: result,
+                    error: null
+                })
+        })
+        .catch(err => {
+            res.status(400)
+                .json({
+                    status: 'failed',
+                    data: null,
+                    error: err.message
+                })
+        })
     }
 
-    storeDetailEntity = async (req) => {
-        let result = await DetailEntity.create({
+    update = async (req, res) => {
+        try {
+            let {data, status} = await this.checkExistanceData(req.params.id)
+    
+            if (status == false) {
+                res.status(404)
+                    .json({
+                        status: 'not found!',
+                        data: null,
+                        error: 'not found!'
+                    })
+
+                return;
+            }
+    
+            let requests = this.requestsUpdateEntity(req.body, data.dataValues);
+    
+            let result = await Entity.update({
+                entity_name: requests.entity_name,
+                is_active: requests.is_active,
+                province: requests.province,
+                city_regency: requests.city_regency,
+                sub_regency: requests.sub_regency,
+                address: requests.address,
+            }, {
+                where: {
+                    id: req.params.id
+                }
+            })
+
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: result,
+                    error: null
+                })
+        } catch (error) {
+            res.status(400)
+                .json({
+                    status: 'failed',
+                    data: null,
+                    error: error.message
+                })
+        }
+    }
+
+    activate = async (req, res) => {
+        try {
+            let {data, status} = await this.checkExistanceData(req.params.id)
+
+            if (status == false) {
+                res.status(200)
+                    .json({
+                        status: 'not found!',
+                        data: null,
+                        error: 'not found!'
+                    })
+                
+                return;
+            }
+
+            let result = await Entity.update({
+                is_active: (data.dataValues.is_active == true) ? false : true
+            }, {
+                where: {
+                    id: req.params.id
+                }
+            })
+
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: result,
+                    error: null
+                })
+        } catch (error) {
+            res.status(400)
+                .json({
+                    status: 'failed',
+                    data: null,
+                    error: error.message
+                })
+        }
+    }
+
+    storeDetailEntity = (req, res) => {
+        DetailEntity.create({
             entity_id: req.body.entity_id,
             location_id: req.body.location_id,
             is_active: true,
         })
+        .then(result => {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: result,
+                    error: null
+                })
+        })
+        .catch(err => {
+            res.status(400)
+                .json({
+                    status: 'failed',
+                    data: null,
+                    error: err.message
+                })
+        })
+    }
 
-        return result
+    activateDetailEntity = async (req, res) => {
+        try {
+            let {data, status} = await this.checkExistanceDetailEntity(req.params.id)
+
+            if (status == false) {
+                res.status(404)
+                    .json({
+                        status: 'not found!',
+                        data: null,
+                        error: 'not found!'
+                    })
+
+                return;
+            }
+
+            let result = await DetailEntity.update({
+                is_active: (data.is_active == true) ? false : true,
+            }, {
+                where: {
+                    id: req.params.id
+                }
+            })
+
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: result,
+                    error: null
+                })
+        } catch (error) {
+            res.status(400)
+                .json({
+                    status: 'failed',
+                    data: null,
+                    error: error.message
+                })
+        }
+    }
+
+    deleteDetailEntity = async (req, res) => {
+        try {
+            let {status} = await this.checkExistanceDetailEntity(req.params.id)
+
+            if (status == false) {
+                res.status(404)
+                    .json({
+                        status: 'not found!',
+                        data: null,
+                        error: 'not found!'
+                    })
+
+                return;
+            }
+
+            let result = await DetailEntity.destroy({
+                where: {
+                    id: req.params.id
+                }
+            })
+
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: result,
+                    error: null
+                })
+        } catch (error) {
+            res.status(400)
+                .json({
+                    status: 'failed',
+                    data: null,
+                    error: error.message
+                })
+        }
     }
 
     checkExistanceData = async (id) => {
         let result = await Entity.findOne({
-            attributes: ['id'],
             where: {
                 id: id
             }
@@ -68,6 +310,30 @@ class EntityController {
         return {
             data: (result) ? result : null,
             status: (result) ? true : false
+        }
+    }
+
+    checkExistanceDetailEntity = async (id) => {
+        let result = await DetailEntity.findOne({
+            where: {
+                id: id
+            }
+        })
+
+        return {
+            data: (result) ? result.dataValues : null,
+            status: (result) ? true : false
+        }
+    }
+
+    requestsUpdateEntity = (request, dataEntity) => {
+        return {
+            entity_name: (request.entity_name) ? request.entity_name : dataEntity.entity_name,
+            is_active: (request.is_active) ? request.is_active : dataEntity.is_active,
+            province: (request.province) ? request.province : dataEntity.province,
+            city_regency: (request.city_regency) ? request.city_regency : dataEntity.city_regency,
+            sub_regency: (request.sub_regency) ? request.sub_regency : dataEntity.sub_regency,
+            address: (request.address) ? request.address : dataEntity.address,
         }
     }
 }
--- test/app/Controller/School/EntityController.test.js
+++ test/app/Controller/School/EntityController.test.js
@@ -19,7 +19,7 @@ test('test to get detail data', async () => {
 it('will create new entity', async () => {
     let result = await EntityController.store({
         body: {
-            entity_name: 'Pondok Pesantren Fajrussalam 2'
+            entity_name: 'Pondok Pesantren Fajrussalam 3'
         }
     })
 
@@ -28,25 +28,99 @@ it('will create new entity', async () => {
     expect(result).toBeDefined()
 })
 
-let tables = [
-    {
-        entity_id: 6,
-        location_id: 19
-    }, {
-        entity_id: 6,
-        location_id: 20
-    }
-]
+it('test update entity in EntityController', async () => {
+    let callback = jest.fn();
 
-it.only.each(tables)('test to input detail entity', async ({entity_id, location_id}) => {
-    let result = await EntityController.storeDetailEntity({
+    await EntityController.update({
         body: {
-            entity_id: entity_id,
-            location_id: location_id
+            entity_name: 'Pondok Pesantren Fajrussalam',
+            is_active: true,
+            // province: 'Jawa Barat',
+            // city_regency: 'Kabupaten Bogor',
+            sub_regency: 'Babakan Madang',
+            address: 'Karang Tengah',
+        },
+        params: {
+            id: 4
+        }
+    }, callback);
+
+    console.info(callback.mock.calls[0]);
+
+    expect(callback).toHaveBeenCalled();
+})
+
+it('test activate EntityController', async () => {
+    let callback = jest.fn();
+
+    await EntityController.activate({
+        params: {
+            id: 4
+        }
+    }, callback)
+
+    console.info(callback.mock.calls[0])
+
+    expect(callback).toHaveBeenCalled();
+})
+
+it('test to input detail entity', () => {
+    let callback = jest.fn();
+
+    callback.mockImplementation(() => {
+        return {
+            entity_id: 8,
+            location_id: 13,
+            is_active: true
         }
     })
 
-    console.info(result)
+    EntityController.storeDetailEntity({
+        body: {
+            entity_id: 8,
+            location_id: 13,
+        }
+    }, callback)
 
-    expect(result).toBeDefined();
+    console.info(callback.mock.results[0].value)
+
+    expect(callback).toHaveBeenCalledWith({
+        entity_id: 8,
+        location_id: 13,
+        is_active: true
+    });
+})
+
+it('test to activate detail entity', async () => {
+    let callback = jest.fn();
+
+    callback.mockImplementation(() => {
+        return 1;
+    })
+
+    await EntityController.activateDetailEntity({
+        params: {
+            id: 13
+        }
+    }, callback)
+
+    expect(callback).toHaveBeenCalledWith(1);
+})
+
+test.only('test delete detail entity', async () => {
+    let callback = jest.fn();
+
+    callback.mockImplementation(() => {
+        return 1;
+    })
+
+    await EntityController.deleteDetailEntity({
+        params: {
+            id: 13
+        }
+    }, callback)
+
+    console.info(callback.mock.results[0].value);
+
+    expect(callback).toHaveBeenCalledWith(1);
 })
